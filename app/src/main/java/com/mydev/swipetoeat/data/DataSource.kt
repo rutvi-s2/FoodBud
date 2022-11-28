3d87e3f38c04d5234def4c4ca6354585a6597a06
rutvi-s2 <rutvi01.shah@gmail.com>
2022-11-28 16:45:02 -0600

    swipe fix added

--- app/src/main/java/com/mydev/swipetoeat/data/DataSource.kt
+++ app/src/main/java/com/mydev/swipetoeat/data/DataSource.kt
@@ -3,7 +3,7 @@ package com.mydev.swipetoeat.data
 import com.mydev.swipetoeat.YelpRestaurant
 
 /**
- * An object to generate a static list of movie reviews from user
+ * An object to generate different restaurant variables depending on user input
  */
 object DataSource {
 
@@ -13,4 +13,6 @@ object DataSource {
     var cuisineAlias = listOf("italian", "chinese", "indpak", "mexican", "tradamerican", "french", "greek", "thai", "japanese", "turkish", "moroccan", "korean", "german", "southafrican", "lebanese", "portuguese", "vietnamese", "belgian", "pakistani", "australian", "brazilian", "himalayan", "russian", "filipino", "peruvian", "ethiopian")
 
     var swipedRightRestaurants: MutableList<YelpRestaurant> = mutableListOf()
+
+    var countSwiped = 0
 }
